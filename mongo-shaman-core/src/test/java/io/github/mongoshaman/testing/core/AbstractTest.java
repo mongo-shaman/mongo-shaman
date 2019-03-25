1ac60d7ede26dfe152dcebdf492400d19cd17e03
Antonio Vizuete <antonio.vizuete.sanchis@everis.com>
2019-03-25 16:09:05 +0100

    Added de.flapdoodle.embed.mongo testing dependency

--- mongo-shaman-core/src/test/java/io/github/mongoshaman/testing/core/AbstractTest.java
+++ mongo-shaman-core/src/test/java/io/github/mongoshaman/testing/core/AbstractTest.java
@@ -4,25 +4,63 @@ import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
+import de.flapdoodle.embed.mongo.MongodExecutable;
+import de.flapdoodle.embed.mongo.MongodProcess;
+import de.flapdoodle.embed.mongo.MongodStarter;
+import de.flapdoodle.embed.mongo.config.IMongodConfig;
+import de.flapdoodle.embed.mongo.config.MongodConfigBuilder;
+import de.flapdoodle.embed.mongo.config.Net;
+import de.flapdoodle.embed.mongo.distribution.Version;
+import de.flapdoodle.embed.process.runtime.Network;
 import io.github.mongoshaman.core.configuration.ShamanProperties;
 import org.bson.Document;
+import org.junit.After;
 import org.junit.Assert;
+import org.junit.Before;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public abstract class AbstractTest {
-  protected static final String TEST = "shaman-test";
-  protected static final String CONNECTION_STRING = "mongodb://localhost:27017/" + TEST + "?replicaSet=rs0";
+  private static final String DB_NAME = "shaman-test";
+  private static final String HOST_NAME = "localhost";
+  private static final int PORT = 12345;
 
-  protected MongoClient mongoClient = MongoClients.create(CONNECTION_STRING);
+
+  protected static final String CONNECTION_STRING = "mongodb://"+HOST_NAME+":"+PORT+"/" + DB_NAME;
+  protected MongoClient mongoClient;
+
+  private MongodExecutable mongodExe;
+  private MongodProcess mongod;
+
+  @Before
+  public void beforeEach() throws Exception {
+    MongodStarter starter = MongodStarter.getDefaultInstance();
+    String bindIp = "localhost";
+    int port = 12345;
+    IMongodConfig mongodConfig = new MongodConfigBuilder()
+      .version(Version.Main.PRODUCTION)
+      .net(new Net(bindIp, port, Network.localhostIsIPv6()))
+      .build();
+    this.mongodExe = starter.prepare(mongodConfig);
+    this.mongod = mongodExe.start();
+    this.mongoClient = MongoClients.create(CONNECTION_STRING);
+  }
+
+  @After
+  public void afterEach() throws Exception {
+    if (this.mongod != null) {
+      this.mongod.stop();
+      this.mongodExe.stop();
+    }
+  }
 
   protected static void globalInit() {
     setDatabaseNameProperty();
   }
 
   protected static void setDatabaseNameProperty() {
-    System.setProperty(ShamanProperties.DATABASE_NAME.getKey(), TEST);
+    System.setProperty(ShamanProperties.DATABASE_NAME.getKey(), DB_NAME);
   }
 
   protected MongoDatabase getDatabase() {
